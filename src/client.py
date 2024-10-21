367b716c01290f66b5d1ad7e64b3a7be4d5ccf34
SergeyRomanov9896 <automag2016@mail.ru>
2024-10-21 18:06:09 +0300

    making changes

--- src/client.py
+++ src/client.py
@@ -1,8 +1,17 @@
 import os
-from src.utils.load_data import load_data
+
+from src.utils.last_5_executed import last_5_executed
 
 DATA_JSON = os.path.join("C:/Users/Admin/PycharmProjects/Operations/data/operations.json")
 
 def clients_operations():
-    """Выводит последние 5 операций клиента"""
-    pass
+    """Виджет, который показывает несколько последних успешных банковских операций клиента"""
+    last_5 = last_5_executed(DATA_JSON)
+
+    last_5.sort(reverse=True, key=lambda x: x['date'])
+    for date in last_5:
+        pruning = date["date"][:-16]
+        point = pruning.replace("-", ".")
+        print(point)
+
+clients_operations()
